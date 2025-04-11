5d2d8b378281158ab78813b4e847373ca201f4e4
Jay Thakur <jthakur@habana.ai>
2025-04-11 17:13:13 +0530

    [SW-223847]: Fix sgl_kernel module not available (#2)
    * [SW-223847]: import awq_dequantize if cuda avaialble
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: vikram singh shekhawat <vshekhawat@habana.ai>

--- python/sglang/srt/layers/quantization/awq.py
+++ python/sglang/srt/layers/quantization/awq.py
@@ -3,7 +3,11 @@ import logging
 from typing import Any, Dict, List, Optional, Union
 
 import torch
-from sgl_kernel import awq_dequantize
+
+try:
+    from sgl_kernel import awq_dequantize
+except ImportError:
+    pass
 
 from sglang.srt.layers.linear import (
     LinearBase,
