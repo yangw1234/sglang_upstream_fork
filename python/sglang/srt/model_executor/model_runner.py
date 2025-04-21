b75996ccba79e17a35ed644788893ef52042d18a
vikram singh shekhawat <vshekhawat@habana.ai>
2025-04-21 16:10:02 +0530

    Fix CLANG pre_commit for model_runner.py. (#16)

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -440,7 +440,9 @@ class ModelRunner:
             )
 
         if self.server_args.enable_torch_compile:
-            self.model = torch.compile(self.model, backend=get_compiler_backend(), dynamic=False)
+            self.model = torch.compile(
+                self.model, backend=get_compiler_backend(), dynamic=False
+            )
 
         monkey_patch_vllm_parallel_state(reverse=True)
         monkey_patch_isinstance_for_vllm_base_layer(reverse=True)
